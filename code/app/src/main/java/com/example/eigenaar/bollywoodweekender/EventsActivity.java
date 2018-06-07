17b73f3d488645eedb8884bb57e974933893a0b9
P-SP <31902611+P-SP@users.noreply.github.com>
2018-06-07 19:46:13 +0200

    Code app. Er gaat iets mis met het VCS van Android Studio, dus daarom zo.

---
+++ code/app/src/main/java/com/example/eigenaar/bollywoodweekender/EventsActivity.java
@@ -0,0 +1,79 @@
+package com.example.eigenaar.bollywoodweekender;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Eigenaar on 7-6-2018.
+ */
+
+public class EventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.home){
+            // go to next activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.eventsLoc){
+            // go to next activity
+            Intent intent = new Intent(this, EventsLocActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.weather){
+            // go to next activity
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.news){
+            // go to next activity
+            Intent intent = new Intent(this, NewsActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.activities){
+            // go to next activity
+            Intent intent = new Intent(this, EventsActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.program){
+            // go to next activity
+            Intent intent = new Intent(this, ProgrActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.map){
+            // go to next activity
+            Intent intent = new Intent(this, MapActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.idea){
+            // go to next activity
+            Intent intent = new Intent(this, IdeaActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showAct(View v) {
+        // go to next activity
+        Intent intent = new Intent(this, EventActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ code/app/src/main/java/com/example/eigenaar/bollywoodweekender/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.eigenaar.bollywoodweekender;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//
+//        menu.clear(); // Clear the menu first
+//
+//            /* Add the menu items */
+//        return super.onPrepareOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.home){
+            // go to next activity
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.eventsLoc){
+            // go to next activity
+            Intent intent = new Intent(this, EventsLocActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.weather){
+            // go to next activity
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.news){
+            // go to next activity
+            Intent intent = new Intent(this, NewsActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.activities){
+            // go to next activity
+            Intent intent = new Intent(this, EventsActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.program){
+            // go to next activity
+            Intent intent = new Intent(this, ProgrActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.map){
+            // go to next activity
+            Intent intent = new Intent(this, MapActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.idea){
+            // go to next activity
+            Intent intent = new Intent(this, IdeaActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
