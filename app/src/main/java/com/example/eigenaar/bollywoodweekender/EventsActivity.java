3e59b8d6093fbf18f69edcb4fa6b69543b63da13
P-SP <31902611+P-SP@users.noreply.github.com>
2018-06-09 20:57:34 +0200

    Menu heeft nu alles

--- app/src/main/java/com/example/eigenaar/bollywoodweekender/EventsActivity.java
+++ app/src/main/java/com/example/eigenaar/bollywoodweekender/EventsActivity.java
@@ -1,25 +1,28 @@
 package com.example.eigenaar.bollywoodweekender;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Eigenaar on 9-6-2018.
+ */
+
+public class EventsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_events);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -65,5 +68,11 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showAct(View v) {
+        // go to next activity
+        Intent intent = new Intent(this, EventActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/eigenaar/bollywoodweekender/NewsActivity.java
+++ app/src/main/java/com/example/eigenaar/bollywoodweekender/NewsActivity.java
@@ -1,65 +1,67 @@
 package com.example.eigenaar.bollywoodweekender;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Eigenaar on 7-6-2018.
+ */
+
+public class NewsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_news);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.home){
+        if(item.getItemId()== R.id.home){
             // go to next activity
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
-        if(item.getItemId()==R.id.eventsLoc){
+        if(item.getItemId()== R.id.eventsLoc){
             // go to next activity
             Intent intent = new Intent(this, EventsLocActivity.class);
             startActivity(intent);
         }
-        if(item.getItemId()==R.id.weather){
+        if(item.getItemId()== R.id.weather){
             // go to next activity
             Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
         }
-        if(item.getItemId()==R.id.news){
+        if(item.getItemId()== R.id.news){
             // go to next activity
             Intent intent = new Intent(this, NewsActivity.class);
             startActivity(intent);
         }
-        if(item.getItemId()==R.id.activities){
+        if(item.getItemId()== R.id.activities){
             // go to next activity
             Intent intent = new Intent(this, EventsActivity.class);
             startActivity(intent);
         }
-        if(item.getItemId()==R.id.program){
+        if(item.getItemId()== R.id.program){
             // go to next activity
             Intent intent = new Intent(this, ProgrActivity.class);
             startActivity(intent);
         }
-        if(item.getItemId()==R.id.map){
+        if(item.getItemId()== R.id.map){
             // go to next activity
             Intent intent = new Intent(this, MapActivity.class);
             startActivity(intent);
         }
-        if(item.getItemId()==R.id.idea){
+        if(item.getItemId()== R.id.idea){
             // go to next activity
             Intent intent = new Intent(this, IdeaActivity.class);
             startActivity(intent);
